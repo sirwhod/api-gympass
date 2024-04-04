99fbda76dd5dd679007613557293b888d60ab09c
Rodrigo <rodrigo.brandao98@gmail.com>
2024-04-04 16:52:13 -0300

    class: Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
+const prisma = new PrismaClient()
+
 export const app = fastify()
+
+prisma.user.create({
+  data: {
+    name: 'Rodrigo Brandão',
+    email: 'rodrigo.brandao98@gmail.com',
+  },
+})
