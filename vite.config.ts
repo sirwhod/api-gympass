2153acc236607ea727a5bddcf230c364133e6f7f
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-04-11 21:47:40 -0300

    class: Criando test environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [
+      ['src/http/controllers/**', './vitest-environment/prisma.ts'],
+    ],
+    dir: 'src', // Essa linha
+  },
 })
